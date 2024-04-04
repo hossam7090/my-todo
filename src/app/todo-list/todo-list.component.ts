c4cb9ed6525ff02dce4801c7fbf77293bff9d239
Khaled Mahmoud Abu-3rida <khaledabuarida0@gmail.com>
2024-04-04 09:26:37 +0200

    with text area

--- src/app/todo-list/todo-list.component.ts
+++ src/app/todo-list/todo-list.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 
 
+
+
 interface Itask{
   id: number;
   title:string;
   description:string;
   status:boolean;
-  date:Date | string;
+  date: string;
 }
+
 @Component({
   selector: 'app-todo-list',
   templateUrl: './todo-list.component.html',
@@ -20,20 +23,64 @@ export class TodoListComponent {
   title: string = ''; // Declare title property
   description: string ='';
   date :string=new Date().toLocaleDateString();
+  id: number = new Date().getTime();
+
   todos: Itask[] = [];
-  
-//TODO handel valdiations 
-//TODO clear date feild after summit
+
+  getCurrentDate() {
+    const currentDate = new Date();
+    const month = (currentDate.getMonth() + 1).toString().padStart(2, '0'); // Adding 1 to month since it's zero-based index
+    const day = currentDate.getDate().toString().padStart(2, '0');
+    const year = currentDate.getFullYear();
+
+  return `${month}-${day}-${year}`;
+}
+
+  validateId = (id: number) => {
+    const isExist = this.todos.findIndex((todo) => {
+      return todo.id === id
+    })    
+    console.log("isExist: ",isExist);
+    return isExist;
+  }
+
   addTodo() {
-    if (this.title.trim() !== ''&&this.description.trim() !== '') {
-      this.todos.push({
-        id: new Date().getTime(), title: this.title, description: this.description,
-        status: false,
-        date: this.date
-      });
-      this.title = ''; 
-      this.description = ''; 
+
+    // check if the todo is exist
+    const todoIndex = this.validateId(this.id);
+
+    // if the todo is exist => UPDATE
+    if(todoIndex != -1){
+      this.todos[todoIndex].title = this.title;
+      this.todos[todoIndex].description = this.description;
+      this.todos[todoIndex].date = this.date;
     }
+    else {
+      if (this.title.trim() !== '' && this.description.trim() !== '' && this.date.trim() !== '') {
+        this.todos.push({
+          id: new Date().getTime(), title: this.title, description: this.description,
+          status: false,
+          date: this.date
+        });
+      }  
+    }
+     
+    // clear input fields
+    this.title = ''; 
+    this.description = ''; 
+    this.date = ''
+    this.id = -2
+  }
+
+  editTodo(index: number){
+    const editedTodo = this.todos[index]
+    this.title = editedTodo.title
+    this.description = editedTodo.description
+    this.date = editedTodo.date
+    this.id = editedTodo.id
+
+    console.log(this.getCurrentDate())
+  
   }
 
   deleteTodo(index: number) {
