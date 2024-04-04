e922c9217cfb247c6cb3de3d28c315b8ab42d6d0
hossam7090 <99748154+hossam7090@users.noreply.github.com>
2024-04-04 06:52:48 +0200

    handel date and status

--- src/app/todo-list/todo-list.component.ts
+++ src/app/todo-list/todo-list.component.ts
@@ -4,7 +4,9 @@ import { Component } from '@angular/core';
 interface Itask{
   id: number;
   title:string;
-  description:string
+  description:string;
+  status:boolean;
+  date:Date | string;
 }
 @Component({
   selector: 'app-todo-list',
@@ -17,12 +19,18 @@ interface Itask{
 export class TodoListComponent {
   title: string = ''; // Declare title property
   description: string ='';
+  date :string=new Date().toLocaleDateString();
   todos: Itask[] = [];
   
-
+//TODO handel valdiations 
+//TODO clear date feild after summit
   addTodo() {
     if (this.title.trim() !== ''&&this.description.trim() !== '') {
-      this.todos.push({id:new Date().getTime(),title:this.title,description:this.description});
+      this.todos.push({
+        id: new Date().getTime(), title: this.title, description: this.description,
+        status: false,
+        date: this.date
+      });
       this.title = ''; 
       this.description = ''; 
     }
